c81f31666c74ed4e063e972e9adec3153326d41b
秦浩钧 <bolvvv@qinhaojundeMBP.lan>
2019-06-09 12:57:41 +0800

    注释去除完成

--- back/src/main/java/cpcourse/back/Service/ServiceImpl/GramaticalAnalysisImpl.java
+++ back/src/main/java/cpcourse/back/Service/ServiceImpl/GramaticalAnalysisImpl.java
@@ -5,6 +5,7 @@ import cpcourse.back.Service.GrammaticalAnalysis;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Stack;
 
 @Service
 public class GramaticalAnalysisImpl implements GrammaticalAnalysis {
@@ -15,7 +16,37 @@ public class GramaticalAnalysisImpl implements GrammaticalAnalysis {
         for(int i = 0, j = 0; i < lexicalAnalysisString.size(); i+=2, j++){
             tokens[j] = Integer.valueOf(lexicalAnalysisString.get(i));
         }
-        RecursionCore recursionCore = new RecursionCore(tokens);
+        //去除注释
+        Stack<Integer> st = new Stack<Integer>();
+        int[] cleanTokens = {};
+        for(int i = 0; i < tokens.length; i++){
+            if(tokens[i] == 25) st.push(i);
+            else if(tokens[i] == 26){
+                int begin = st.pop();
+                deleteArrays(tokens, begin, i);
+            }
+        }
+        if(!st.empty()) {
+            System.out.println("注释错误");
+            return null;
+        }
+        else {
+            int count = 0;
+            for(int i = 0; i < tokens.length; i++){
+                if(tokens[i] == -1) count++;
+            }
+            cleanTokens = new int[tokens.length - count];
+            int j = 0;
+            for(int i = 0; i < tokens.length; i++){
+                if(tokens[i] != -1){
+                    cleanTokens[j] = tokens[i];
+                    j++;
+                }
+            }
+        }
+
+        //进行语法分析
+        RecursionCore recursionCore = new RecursionCore(cleanTokens);
         try{
             if(recursionCore.program1()) System.out.println("语法无错误");
             else {
@@ -24,9 +55,18 @@ public class GramaticalAnalysisImpl implements GrammaticalAnalysis {
 
             }
         }catch (Exception e){
-            System.out.println("语法错误，指针已到末尾");
+            System.out.println("语法错误");
+            System.out.println("语法错误位置为"+recursionCore.errorPointer+"附近");
         }
         return null;
     }
 
+    //去除注释间的内容，将原有的内容设置为-1
+    private void deleteArrays(int[] source, int begin, int end){
+        for(int i = 0; i < source.length; i++){
+            if(i>=begin&&i<=end) {
+                source[i] = -1;
+            }
+        }
+    }
 }
